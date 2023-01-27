c7bc4c8bf7ed32b346bb5b31e51e30b24553f8c5
Vijay Kotagiri <kotagiri@mo.ece.pdx.edu>
2023-01-27 01:41:13 -0800

    final_submit_lab2

--- fifo1_srama.design_config.tcl
+++ fifo1_srama.design_config.tcl
@@ -20,7 +20,7 @@ set lib_dir /pkgs/synopsys/2020/32_28nm/SAED32_EDK/lib
 # ICC2 NDM choosing also uses the sub_lib_types so that only the required libraries and extras are not loaded.
 
 # fifo1
-#set top_design fifo1
+#set top_design fifo
 set add_ios 1
 set pad_design 1
 set design_size { 580 580  } 
@@ -30,7 +30,7 @@ set enable_dft 0
 set innovus_enable_manual_macro_placement 0
 
 set rtl_list [list ../rtl/$top_design.sv ]
-set slow_corner "ss0p95v125c_2p25v ss0p95v125c"
+set slow_corner "ss0p95v125c_2p25v ss0p95v125c ss0p75v125c_i0p75v"
 set fast_corner "ff0p95vn40c ff1p16vn40c_2p75v ff1p16vn40c"
 set synth_corners $slow_corner
 set synth_corners_target "ss0p95v125c" 
@@ -39,27 +39,27 @@ set synth_corners_slow $slow_corner
 set synth_corners_fast $fast_corner
 set slow_metal Cmax_125
 set fast_metal Cmax_125
-set lib_types "$lib_dir/io_std/db_nldm $lib_dir/sram/db_nldm $lib_dir/pll/db_nldm"
-set ndm_types "$lib_dir/stdcell_rvt/ndm $lib_dir/stdcell_hvt/ndm $lib_dir/sram/ndm $lib_dir/io_std/ndm  $lib_dir/pll/ndm"
+set lib_types "$lib_dir/io_std/db_nldm $lib_dir/sram_lp/db_nldm $lib_dir/pll/db_nldm"
+set ndm_types "$lib_dir/stdcell_rvt/ndm $lib_dir/stdcell_hvt/ndm $lib_dir/sram_lp/ndm $lib_dir/io_std/ndm  $lib_dir/pll/ndm"
 set lib_types_target "$lib_dir/stdcell_rvt/db_nldm"
-# Get just the main standard cells, srams and IOs
-set sub_lib_type "saed32?vt_ saed32sram_ saed32io_wb_ saed32pll_"
+# Get just the main standard cells, sram_lps and IOs
+set sub_lib_type "saed32?vt_ saed32sramlp_ saed32io_wb_ saed32pll_"
 set sub_lib_type_target "saed32rvt_"
 
 set lef_types [list $lib_dir/stdcell_hvt/lef  \
 $lib_dir/stdcell_rvt/lef \
 $lib_dir/stdcell_lvt/lef \
-$lib_dir/sram/lef/ \
+$lib_dir/sram_lp/lef/ \
 $lib_dir/io_std/lef \
 $lib_dir/pll/lef \
 ]
 
-set sub_lef_type "saed32nm_?vt_*.lef saed32_sram_*.lef saed32io_std_wb saed32_PLL.lef"
+set sub_lef_type "saed32nm_?vt_*.lef saed32_sramlp_*.lef saed32io_std_wb saed32_PLL.lef"
 set mwlib_types [list $lib_dir/stdcell_hvt/milkyway \
 $lib_dir/stdcell_rvt/milkyway \
 $lib_dir/stdcell_lvt/milkyway  \
 $lib_dir/io_std/milkyway \
-$lib_dir/sram/milkyway $lib_dir/pll/milkyway \
+$lib_dir/sram_lp/milkyway $lib_dir/pll/milkyway \
  ]
 set sub_mwlib_type "saed32nm_?vt_* SRAM32NM saed32io_wb_* SAED32_PLL_FR*"
 
